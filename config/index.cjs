af4708dcbf5eb62fd6ccd8dc902afb469e77cea9
MapleLeaf <475602529@qq.com>
2022-11-05 14:19:26 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,7 +6,8 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-
+  
+  USE_PASSAGE:'push-push',
   // 使用微信测试号：公众号APP_ID
   APP_ID: '',
 
@@ -21,11 +22,11 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: '435df2f508e6494d85dcb5ca6a3fbb03',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '12-5',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
